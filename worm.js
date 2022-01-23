8e88b8658e34516652aaad9f0a2ca4c2f1fd3c1c
Dash <aaronfillmore18@gmail.com>
2022-01-23 13:07:22 -0800

    Small comment addition

--- worm.js
+++ worm.js
@@ -1,6 +1,8 @@
+// Custom little thing for Bitburner's script editor to use some intellicomplete thing, idk, RTFM.
 /**
 * @param {NS} ns
 **/
+
 
 // Array of who to iterate through and smack around. I was going to automate the gathering but the scan() function doesn't have a depth param, so this is easier for now. Don't feel like writing a wholeass worm yet.
 const patheticVictims = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "iron-gym", "max-hardware", "zer0", "nectar-net", "CSEC", "neo-net", "silver-helix", "omega-net", "phantasy", "avmnite-02h","the-hub", "comptek", "netlink", "johnson-ortho", "crush-fitness", "rothman-uni", "catalyst", "zb-institute", "I.I.I.I", "summit-uni"];
