3305ce8300ed9c19ee7d5341ca70f4ee74b46501
Dash <aaronfillmore18@gmail.com>
2022-01-24 20:28:57 -0800

    Adjusted sleep position so it runs scripts first

--- scheduler.js
+++ scheduler.js
@@ -7,8 +7,8 @@
 export async function main(ns) {
   // Infinite loop, of course. I actually screwed this up and froze the game, which netted me an achievement. Sucking isn't all bad!
   while(true) { 
-    await ns.sleep(600000); // Sleep for 10 minutes because we don't need to be butt blasting the game with this crap.
     await ns.run("worm.js"); // Deploy our turbo worm to root servers.
     await ns.run("hackDeploy.js"); // Deploy our hack script to the heretics and execute them with threads.
+    await ns.sleep(600000); // Sleep for 10 minutes because we don't need to be butt blasting the game with this crap.
   };
 };
